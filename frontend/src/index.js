f21cf643e99b2c594319929021d0a1d5811d585b
skbss1224 <skbss1224@gmail.com>
2022-03-03 11:41:32 +0530

    project created and executed

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+//render("what to show","where to show")
+ReactDom.render(
+    <>
+    <h1>java full stack</h1>
+    <p>sasi kumar s</p>
+    <h2>hello world</h2>
+    </>
+    ,document.getElementById("root")
+
+)
